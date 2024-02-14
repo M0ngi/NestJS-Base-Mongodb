b49af24f076e216708831f6c3ee7330489e47a62
M0ngi <31113271+M0ngi@users.noreply.github.com>
2024-02-14 05:33:02 +0100

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@app_config/config.service';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: "*"
   })
+  app.useGlobalPipes(new ValidationPipe({
+    stopAtFirstError: true,
+    forbidUnknownValues: true,
+    forbidNonWhitelisted: true,
+    transform: true,
+    whitelist: true,
+  }));
 
   const configService = app.get(ConfigService);
   const PORT = configService.getMiscConfig().port;
